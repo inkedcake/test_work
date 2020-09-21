dbd5e2f4092eb7bb152fbbcbb4a19e841f53759e
inkedcake <korzhenko.alexandr@gmail.com>
2020-09-22 00:48:46 +0300

    add dump db

--- db_dump/work_test.sql
+++ db_dump/work_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 27 2020 г., 20:47
+-- Время создания: Сен 22 2020 г., 00:47
 -- Версия сервера: 5.7.23
 -- Версия PHP: 7.2.10
 
@@ -25,6 +25,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `calendar`
+--
+
+CREATE TABLE `calendar` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `remind_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `tasks`
 --
 
@@ -42,10 +58,8 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `created_by`, `manager_id`, `client_id`, `date_time`, `text_task`) VALUES
-(3, 0, 5, 13, '2020-08-30 06:01:00', 'test'),
-(4, 5, 16, 14, '2020-08-22 22:58:00', 'gvkgvj'),
-(5, 5, 5, 12, '2020-08-14 23:03:00', 'etsefscf'),
-(6, 5, 5, 15, '2020-08-27 00:32:00', 'dzvsdv');
+(12, 5, 16, 12, '2020-09-17 00:41:00', 'test'),
+(14, 5, 5, 12, '2020-09-23 00:46:00', '');
 
 -- --------------------------------------------------------
 
@@ -70,12 +84,17 @@ INSERT INTO `users` (`id`, `full_name`, `login`, `password`, `position`, `phone`
 (5, 'test test  tset', 'etst', '098f6bcd4621d373cade4e832627b4f6', 'manager', 'test@e.g'),
 (6, 'rg', 'erg', '75fe57ec4a047a300cac5f27223df81a', 'customer', 'sef@sef.gy'),
 (12, 'test Testovich', '+38 (050) 660-63-94', 'f93c488106cd8085077f3e41de08d904', 'customer', '+38 (050) 660-63-94'),
-(15, 'test test  tset', '+3 (805) 066 06 39 4', 'c79504b4a3059bcee3822c2f8fa63b66', 'customer', '+3 (805) 066 06 39 4'),
 (16, 'new test', 'etstttt', '098f6bcd4621d373cade4e832627b4f6', 'manager', 'test@e.g');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `calendar`
+--
+ALTER TABLE `calendar`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `tasks`
@@ -94,10 +113,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `calendar`
+--
+ALTER TABLE `calendar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
